c618b2a4de107cb0f008042896256fec55716af3
Suraj <surajk75395@gmail.com>
2026-02-27 12:56:56 +0530

    Add files via upload

---
+++ list & tuple/methods in tuple.py
@@ -0,0 +1,13 @@
+tuple=(1,2,3,4,5)
+# count the number of times 1 appears in the tuple
+print("The number of times 1 appears in the tuple is:",tuple.count(1)) 
+# find the index of the first occurrence of 3 in the
+print("The index of the first occurrence of 3 in the tuple is:",tuple.index(3)) 
+ # find the maximum value in the tuple
+print("The maximum value in the tuple is:",max(tuple))
+ # find the minimum value in the tuple
+print("The minimum value in the tuple is:",min(tuple))
+ # find the length of the tuple
+print("The length of the tuple is:",len(tuple))
+# find the sum of all the elements in the tuple
+print("The sum of all elements in the tuple is:",sum(tuple)) 
