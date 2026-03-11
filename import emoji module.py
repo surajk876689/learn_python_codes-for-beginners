bdc2008ddce86056cc7a5cb6c0243a0c70be080e
Suraj <surajk75395@gmail.com>
2026-03-12 03:06:22 +0530

    Delete import emoji module.py

--- import emoji module.py
+++
@@ -1,5 +0,0 @@
-
-from emoji import emojize
-print(emojize("Python is :thumbs_up:"))
-'''this code will print the text "Python is " followed by a thumbs up emoji.
-You can change the text and the emoji code to display different emojis.'''
