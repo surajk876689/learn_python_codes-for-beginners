9df51f40b0a52cea61003cb493627a9d6278797e
Suraj <surajk75395@gmail.com>
2026-03-12 03:09:52 +0530

    Delete square of two numbers.py

--- square of two numbers.py
+++
@@ -1,4 +0,0 @@
-a=int(input("Enter a number: "))
-square=a**2
-print("The square of the number is:", square)
-'''this code will prompt the user to enter a number, calculate its square, and then print the result.'''
