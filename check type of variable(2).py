a3d2d135fcf3f5009d521670fe1fbc18a8deb351
Suraj <surajk75395@gmail.com>
2026-03-12 03:05:39 +0530

    Delete check type of variable(2).py

--- check type of variable(2).py
+++
@@ -1,4 +0,0 @@
-a=input("Enter a value: ")
-print("The type of the variable a is:", type(a))
-'''this code will prompt the user to enter a value,
-and then it will print the type of the variable 'a'.'''
