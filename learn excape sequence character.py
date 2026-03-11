c811293b696a04e7ba344c47042bcac07f3969c7
Suraj <surajk75395@gmail.com>
2026-03-12 03:08:13 +0530

    Delete learn excape sequence character.py

--- learn excape sequence character.py
+++
@@ -1,4 +0,0 @@
-import webbrowser#this line imports the webbrowser module, which allows you to open web pages in the default browser.
-url = "https://chatgpt.com/c/699ff265-9c74-8323-855c-ee73d578afbc"# this url is use to learn the escape sequence character in python.
-webbrowser.open(url)#this code will open the specified URL in the default web browser.
-'''this code will open the specified URL in the default web browser, which is a resource for learning about escape sequence characters in Python.'''
