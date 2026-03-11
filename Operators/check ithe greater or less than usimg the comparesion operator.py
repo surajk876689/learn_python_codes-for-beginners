864cc1eba5e5ab44afbef2c8d3c30d3e7476d50f
Suraj <surajk75395@gmail.com>
2026-03-12 03:23:14 +0530

    Add files via upload

---
+++ Operators/check ithe greater or less than usimg the comparesion operator.py
@@ -0,0 +1,9 @@
+a=34
+b=80
+if a > b:
+    print("a is greater than b")
+elif a < b:     
+    print("a is less than b")
+else:
+    print("a is equal to b")
+    '''this code will compare the values of a and b, and print whether a is greater than, less than, or equal to b.'''
